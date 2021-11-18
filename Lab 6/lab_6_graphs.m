f013aa29e40a4d835fd892e598f0de314a3c8af1
Bo Bramer <49931047+wuguishifu@users.noreply.github.com>
2021-11-18 11:16:39 -0800

    Added tables to lab 6 data and began working on data anal

--- Lab 6/lab_6_graphs.m
+++ Lab 6/lab_6_graphs.m
@@ -1,4 +1,26 @@
 clc
 clear
 
+% defining data
+m = readtable("exp_6_raw_data.csv");
 
+times = strings(1, 7);
+concentrations = strings(1, 7);
+saturations = strings(1, 7);
+temps = strings(1, 7);
+
+for i = 1:7
+    times{i} = strcat('t', num2str(i), 'time');
+    concentrations{i} = strcat('t', num2str(i), 'concentration');
+    saturations{i} = strcat('t', num2str(i), 'saturation');
+    temps{i} = strcat('t', num2str(i), 'temp');
+end
+
+rpms = [297, 297, 297, 260, 312, 335, 255]; % rpm
+flowrate = [2, 6, 12, 6, 6, 6, 6]; % L/min
+
+%% plot LHS equation 6 vs. time
+clc
+for i=1:7
+    LHS.(i) = 
+end
